7279c0b2ef2b7344001d0222f5080491c6aabc0f
Sine-sipho <125162881+Sine-sipho@users.noreply.github.com>
2025-05-28 23:47:41 +0200

    search/ get functionality using API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector(".current-temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.getElementById("city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "34dcb3cfedt4654990206a21091oab73";
+  let apiLink = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiLink).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.getElementById("search-form-input");
-  let city = document.getElementById("city");
-  city.innerHTML =
-    searchInput.value.charAt(0).toUpperCase() + searchInput.value.slice(1);
+  searchCity(searchInput.value);
   searchInput.value = "";
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSearchSubmit);
+
+searchCity("East London");

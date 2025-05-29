108f1b6ec3b40b9ad1522922c783d5fe5e28ecae
Sine-sipho <125162881+Sine-sipho@users.noreply.github.com>
2025-05-29 17:16:44 +0200

    added weather data

--- src/index.js
+++ src/index.js
@@ -3,6 +3,40 @@ function refreshWeather(response) {
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   let cityElement = document.getElementById("city");
   cityElement.innerHTML = response.data.city;
+  let weatherDescription = document.querySelector("#weather-description");
+  weatherDescription.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  let rainElement = document.querySelector("#rain");
+  rainElement.innerHTML = `${response.data.temperature.pressure}hpa`;
+  let windElement = document.querySelector("#windspeed");
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  timeElement.innerHTML = getDate(date);
+}
+function addZero(number) {
+  if (number < 10) {
+    return `0${number}`;
+  } else {
+    return `${number}`;
+  }
+}
+function getDate(date) {
+  let hour = addZero(date.getHours());
+  let minutes = addZero(date.getMinutes());
+  let weekdays = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = weekdays[date.getDay()];
+  return `${day} ${hour}:${minutes}`;
 }
 
 function searchCity(city) {
